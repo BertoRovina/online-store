c3dbb3f6587990bd052458ccd54f55b23bd18d6f
Humberto Rovina <32313091+BertoRovina@users.noreply.github.com>
2019-03-01 14:12:45 -0500

    Adding client, address and phone

---
+++ src/main/java/com/hrovina/onlinestore/entities/Address.java
@@ -0,0 +1,118 @@
+package com.hrovina.onlinestore.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Address implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String address;
+    private String number;
+    private String additionalAddressInfo;
+    private String area;
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name="client_id")
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name="city_id")
+    private City city;
+
+    public Address() {
+    }
+
+    public Address(Integer id, String address, String number, String additionalAddressInfo, String area, String zipCode, Client client, City city) {
+        this.id = id;
+        this.address = address;
+        this.number = number;
+        this.additionalAddressInfo = additionalAddressInfo;
+        this.area = area;
+        this.zipCode = zipCode;
+        this.client = client;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAdditionalAddressInfo() {
+        return additionalAddressInfo;
+    }
+
+    public void setAdditionalAddressInfo(String additionalAddressInfo) {
+        this.additionalAddressInfo = additionalAddressInfo;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/enums/ClientType.java
@@ -0,0 +1,36 @@
+package enums;
+
+public enum ClientType {
+
+    NATURAL_PERSON(1, "Natural Person"),
+    LEGAL_PERSON(2, "Legal Person");
+
+    private int cod;
+    private String description;
+
+    ClientType(int cod, String description) {
+        this.cod = cod;
+        this.description = description;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static ClientType toEnum(Integer cod){
+        if (cod == null){ return null; }
+
+        for (ClientType type : ClientType.values()){
+            if (cod.equals(type.getCod())){
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("Id invalid: " + cod);
+    }
+
+}
