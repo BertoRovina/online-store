b58ac8937125c78dd2c0b973a3af6c9ee161396e
Humberto Rovina <32313091+BertoRovina@users.noreply.github.com>
2019-03-19 17:39:35 -0400

    Configuration and first commit into heroku

---
+++ src/main/java/com/hrovina/onlinestore/config/devConfig.java
@@ -0,0 +1,30 @@
+package com.hrovina.onlinestore.config;
+
+import com.hrovina.onlinestore.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class devConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!strategy.equals("create")){
+           return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
