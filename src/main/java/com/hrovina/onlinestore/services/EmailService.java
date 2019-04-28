774df68d5b5ba2cb0a84d4007de78c8476f48c24
Humberto Rovina <32313091+BertoRovina@users.noreply.github.com>
2019-04-27 21:26:41 -0400

    Adding html format for the confirmation email

--- src/main/java/com/hrovina/onlinestore/services/EmailService.java
+++ src/main/java/com/hrovina/onlinestore/services/EmailService.java
@@ -3,9 +3,15 @@ package com.hrovina.onlinestore.services;
 import com.hrovina.onlinestore.entities.PurchaseOrder;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(PurchaseOrder obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(PurchaseOrder obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
