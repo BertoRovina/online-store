add60c06c111cd8d46b354c6c3f2437f6a133cf6
Humberto Rovina <32313091+BertoRovina@users.noreply.github.com>
2019-05-12 20:26:01 -0400

    Adding authorization

--- src/main/java/com/hrovina/onlinestore/security/JWTUtil.java
+++ src/main/java/com/hrovina/onlinestore/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.hrovina.onlinestore.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,6 +25,30 @@ public class JWTUtil {
                 .compact();
     }
 
+    public boolean validToken(String token){
+        Claims claims = getClaims(token);
+        if (claims != null){
+            String userName = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+            return userName != null && expirationDate != null && now.before(expirationDate);
+        }
+        return false;
+    }
 
+    public String getUSerName(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null) {
+            return claims.getSubject();
+        }
+        return null;
+    }
 
+    private Claims getClaims(String token) {
+        try{
+            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+        }catch (Exception e){
+            return null;
+        }
+    }
 }
